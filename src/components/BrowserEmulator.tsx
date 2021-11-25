5bc6aa972953476f8b5ee05469e784f1ccf48bbe
Antony1060 <antoniostignjedec@gmail.com>
2021-11-25 23:44:57 +0100

    Prototype hover effects

--- src/components/BrowserEmulator.tsx
+++ src/components/BrowserEmulator.tsx
@@ -2,7 +2,7 @@ import { faLock, faRedo, faStar as solidStar } from "@fortawesome/free-solid-svg
 import { faStar as regularStar } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import styled from "styled-components";
 import pgpKey from "url:/assets/antony.asc"
 import sshKey from "url:/assets/antony.pub"
@@ -14,7 +14,6 @@ type ContactInfo = {
     github: string,
     linkedIn: string
 }
-
 
 const BrowserContainer = styled.div`
     width: 100%;
@@ -137,18 +136,53 @@ const ContactLineContainer = styled.div`
 `
 
 const ContactLineLink = styled.a`
+    position: relative;
     text-decoration: none;
     color: #cb8f76;
 
     &:hover {
         text-decoration: underline;
     }
+
+    @media (min-width: 900px) {
+        &:hover div {
+            display: flex;
+        }
+    }
 `
 
-const ContactLine = ({ keyName, value, href }: { keyName: string, value: string, href: string }) => {
-    const overwriteHref: { [k: string]: string } = {
+type ContactLineDetailsProps = {
+    imageUrl?: string,
+    title: string,
+    description: string
+}
+
+const ContactLine = ({ keyName, value, href }: { keyName: keyof ContactInfo, value: string, href: string }) => {
+    const overwriteHref: { [k in keyof ContactInfo]?: string } = {
         mail: `mailto:${href}`,
         discord: "https://discord.gg/tgHWHWtNeD"
+    }
+
+    const linkDetails: { [key in keyof ContactInfo]?: ContactLineDetailsProps } = {
+        linkedIn: {
+            title: "LinkedIn",
+            description: "12 connection"
+        },
+        twitter: {
+            imageUrl: "https://pbs.twimg.com/profile_images/1462878256613494788/_PpROI0E_400x400.jpg",
+            title: "Twitter",
+            description: "20 followers"
+        },
+        github: {
+            imageUrl: "https://avatars.githubusercontent.com/u/38852274",
+            title: "GitHub",
+            description: "577 contributions in the last year"
+        },
+        discord: {
+            imageUrl: "https://cdn.discordapp.com/avatars/136724729011634176/d53dd32949d8aa1eaae32dbb2f4f5f07.webp",
+            title: "Discord",
+            description: "Life is a stack"
+        }
     }
 
     return (
@@ -157,10 +191,55 @@ const ContactLine = ({ keyName, value, href }: { keyName: string, value: string,
             <span>&#58; </span>
             <span>
                 &quot;
-                <ContactLineLink href={overwriteHref[keyName] ?? (href.startsWith("http") ? href : "#")} target="_blank">{ value }</ContactLineLink>
+                <ContactLineLink href={overwriteHref[keyName] ?? (href.startsWith("http") ? href : "#")} target="_blank">
+                    { value }
+                    {linkDetails[keyName] && <ContactLineDetails {...linkDetails[keyName]!} />}
+                </ContactLineLink>
                 &quot;
             </span>
         </ContactLineContainer>
+    )
+}
+
+const ContactLineDetailsContainer = styled.div`
+    position: absolute;
+    left: 0;
+    top: 1.6rem;
+    display: none;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    padding: 1rem;
+    background-color: #423f3f;
+    color: white;
+    z-index: 2;
+    cursor: default;
+    gap: 0.2rem;
+    width: 360px;
+    font-size: 1rem;
+
+    &:hover {
+        display: none !important;
+    }
+
+    span:first-child {
+        font-size: 1.2rem;
+        color: #93d0f0;
+        text-decoration: none;
+
+        &:hover {
+            text-decoration: underline;
+            cursor: pointer;
+        }
+    }
+`
+
+const ContactLineDetails: FC<ContactLineDetailsProps> = ({ title, description }) => {
+    return (
+        <ContactLineDetailsContainer>
+            <span>{title}</span>
+            <span>{description}</span>
+        </ContactLineDetailsContainer>
     )
 }
 
@@ -305,7 +384,7 @@ const BrowserEmulator = () => {
                         <JsonContent>
                             {Object.entries(contact)
                                 .filter(([ k ]) => k !== "status")
-                                .map(([ k, v ]) => <ContactLine key={k} keyName={k} value={v} href={v} />)}
+                                .map(([ k, v ]) => <ContactLine key={k} keyName={k as keyof ContactInfo} value={v} href={v} />)}
                         </JsonContent>
                         <span>&#125;</span>
                     </JsonFormat>
